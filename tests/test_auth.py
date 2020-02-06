ed23e2f1d9a7784f292413b9f1569615b372b10a
Victor Nguli <victor.joseph@financeplan.biz>
2020-02-06 14:48:24 +0300

    add auth blueprint

---
+++ tests/test_auth.py
@@ -0,0 +1,43 @@
+import unittest
+import json
+from app import create_app, db
+
+
+class AuthTest(unittest.TestCase):
+	"""Test class for authentication blueprint"""
+
+	def setUp(self):
+		"""Set up test environment and variables"""
+		self.app = create_app(config_name = "testing")
+		self.client = self.app.test_client
+		self.user_data = {
+			'email': 'mail@example.com',
+			'password': 'quirky_password'
+		}
+
+		with self.app.app_context():
+			db.session.close()
+			db.drop_all()
+			db.create_all()
+
+	def test_registration(self):
+		"""Test user registration works correcty."""
+		res = self.client().post('/auth/register', data = self.user_data)
+		# get the results returned in json format
+		result = json.loads(res.data.decode())
+		# assert that the request contains a success message and a 201 status code
+		self.assertEqual(result['message'], "You registered successfully.")
+		self.assertEqual(res.status_code, 201)
+
+	def test_already_registered_user(self):
+		"""Test that a user cannot be registered twice."""
+		res = self.client().post('/auth/register', data = self.user_data)
+		self.assertEqual(res.status_code, 201)
+		second_res = self.client().post('/auth/register', data = self.user_data)
+		self.assertEqual(second_res.status_code, 202)
+		# get the results returned in json format
+		result = json.loads(second_res.data.decode())
+		self.assertEqual(
+			result['message'], "User already exists. Please login.")
+
+
